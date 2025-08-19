5f5d33486be97ceb689639581aad215ac7924fef
DeveloperDolon <dolonr718@gmail.com>
2025-08-19 11:06:02 +0600

    widget class form method update

--- pgnyt_articles.php
+++ pgnyt_articles.php
@@ -1,81 +1,128 @@
-<?php 
+<?php
 
-    /**
-     * Plugin Name: WP NY Times Plugin 
-     * Plugin URI:  https://wordpress.org/plugins/pgnyt_articles/
-     * Description: Provides both widgets and shortcodes to help you display NY Times articles on your website.
-     * Version:     1.0
-     * Author:      Dolon Chandra Roy
-     * License:     GPLv2 or later
-     */
+/**
+ * Plugin Name: WP NY Times Plugin 
+ * Plugin URI:  https://wordpress.org/plugins/pgnyt_articles/
+ * Description: Provides both widgets and shortcodes to help you display NY Times articles on your website.
+ * Version:     1.0
+ * Author:      Dolon Chandra Roy
+ * License:     GPLv2 or later
+ */
 
-    $plugin_url = WP_PLUGIN_URL . '/pgnyt-articles';
-    $options = array();
+$plugin_url = WP_PLUGIN_URL . '/pgnyt-articles';
+$options = array();
 
-    function pgnyt_articles_menu() {
-        add_options_page(
-            "NY Times Plugin",
-            "NY Times Articles",
-            "manage_options",
-            "pgnyt-articles",
-            "pgnyt_articles_options_page",
-        );
+function pgnyt_articles_menu()
+{
+    add_options_page(
+        "NY Times Plugin",
+        "NY Times Articles",
+        "manage_options",
+        "pgnyt-articles",
+        "pgnyt_articles_options_page",
+    );
+}
+
+add_action("admin_menu", "pgnyt_articles_menu");
+
+function pgnyt_articles_options_page()
+{
+    if (!current_user_can("manage_options")) {
+        wp_die('You don\'t have enough permission to view this page');
     }
 
-    add_action("admin_menu", "pgnyt_articles_menu");
+    global $plugin_url;
+    global $options;
 
-    function pgnyt_articles_options_page() {
-        if(!current_user_can("manage_options")) {
-            wp_die('You don\'t have enough permission to view this page');
+    if (isset($_POST['pgnyt_form_submitted'])) {
+        $hidden_field = esc_html($_POST['pgnyt_form_submitted']);
+
+        if ($hidden_field == 'Y') {
+            $pgnyt_search = esc_html($_POST['pgnyt_search']);
+            $pgnyt_apikey = esc_html($_POST['pgnyt_apikey']);
+
+            $pgnyt_results = pgnyt_articles_get_results($pgnyt_search, $pgnyt_apikey);
+
+            $options['pgnyt_search'] = $pgnyt_search;
+            $options['pgnyt_apikey'] = $pgnyt_apikey;
+            $options['last_updated'] = time();
+
+            $options['pgnyt_results'] = $pgnyt_results;
+
+            update_option('pgnyt_articles', $options);
         }
-
-        global $plugin_url;
-        global $options;
-
-        if(isset($_POST['pgnyt_form_submitted'])) {
-            $hidden_field = esc_html($_POST['pgnyt_form_submitted']);
-
-            if($hidden_field == 'Y') {
-                $pgnyt_search = esc_html($_POST['pgnyt_search']);
-                $pgnyt_apikey = esc_html($_POST['pgnyt_apikey']);
-
-                $pgnyt_results = pgnyt_articles_get_results($pgnyt_search, $pgnyt_apikey);
-
-                $options['pgnyt_search'] = $pgnyt_search; 
-                $options['pgnyt_apikey'] = $pgnyt_apikey;
-                $options['last_updated'] = time();
-
-                $options['pgnyt_results'] = $pgnyt_results;
-            
-                update_option('pgnyt_articles', $options);
-            }
-        }
-
-        $options = get_option( 'pgnyt_articles');
-
-        if($options != '') {
-            $pgnyt_search = $options['pgnyt_search'];
-            $pgnyt_apikey = $options['pgnyt_apikey'];
-            $pgnyt_results = $options['pgnyt_results'];
-        }
-
-        require('inc/options-page-wrapper.php');
     }
 
-    function pgnyt_articles_get_results($pgnyt_search, $pgnyt_apikey) {
-        $json_feed_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" . $pgnyt_search . "&api-key=" . $pgnyt_apikey;
+    $options = get_option('pgnyt_articles');
 
-        $json_feed = wp_remote_get($json_feed_url);
-
-        $pgnyt_results = json_decode($json_feed['body']);
-
-        return $pgnyt_results;
+    if ($options != '') {
+        $pgnyt_search = $options['pgnyt_search'];
+        $pgnyt_apikey = $options['pgnyt_apikey'];
+        $pgnyt_results = $options['pgnyt_results'];
     }
 
-    function pgnyt_articles_backend_styles() {
-        wp_enqueue_style("pgnyt_articles_backend_css", plugins_url( 'pgnyt_articles/pgnyt-articles.css' ));
+    require('inc/options-page-wrapper.php');
+}
+
+class Pgnyt_Articles_Widget extends WP_Widget
+{
+    public function __construct()
+    {
+        // actual widget processes
     }
 
-    add_action( 'admin_head', "pgnyt_articles_backend_styles");
+    public function widget($args, $instance)
+    {
+        // outputs the content of the widget
+        extract($args);
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $num_articles = $instance['num_articles'];
+        $display_image = $instance['display_image'];
+        $options = get_option('pgnyt_articles');
+    }
+
+    public function form($instance) 
+    {
+        // outputs the options form in the admin
+        $title = esc_attr($instance['title']);
+    	$display_image = esc_attr($instance['display_image']);
+    	$num_articles = esc_attr($instance['num_articles']);
+
+    	$options = get_option('pgnyt_articles');
+    	$pgnyt_results = $options['pgnyt_results'];
+
+    	require ('inc/widget-fields.php');
+
+    }
+
+    public function update($new_instance, $old_instance)
+    {
+        // processes widget options to be saved
+    }
+}
+
+add_action( "widgets_init", "wpdocs_register_widgets" );
+
+function wpdocs_register_widgets() {
+    register_widget( 'WPDocs_New_Widget' );
+}
+
+function pgnyt_articles_get_results($pgnyt_search, $pgnyt_apikey)
+{
+    $json_feed_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" . $pgnyt_search . "&api-key=" . $pgnyt_apikey;
+
+    $json_feed = wp_remote_get($json_feed_url);
+
+    $pgnyt_results = json_decode($json_feed['body']);
+
+    return $pgnyt_results;
+}
+
+function pgnyt_articles_backend_styles()
+{
+    wp_enqueue_style("pgnyt_articles_backend_css", plugins_url('pgnyt_articles/pgnyt-articles.css'));
+}
+
+add_action('admin_head', "pgnyt_articles_backend_styles");
 
 ?>
